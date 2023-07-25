e3ce2bc5f1b53d3d964ea42f3a8106de650722d6
Bushra <boushra.m.da@gmail.com>
2023-07-26 02:21:54 +0300

    Adding AutoMapper

---
+++ UnitTestingLearn/BuisnessLayer/Helpers/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UnitTestingLearn.DataLayer.Dtos;
+using UnitTestingLearn.DataLayer.Models;
+
+namespace UnitTestingLearn.BuisnessLayer
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CategoryAddDto, Category>()
+                .ForMember(dest => dest.Image, opt => opt.Ignore());
+        }
+    }
+}
