ce4fa252a2bc8cae35e4da7f36b99978104b13f7
Bushra <boushra.m.da@gmail.com>
2023-07-26 02:27:10 +0300

    Add Services to Program File

--- UnitTestingLearn/Program.cs
+++ UnitTestingLearn/Program.cs
@@ -1,5 +1,9 @@
+using ECommerceAPI.BusinessLayer.Helpers;
+using ECommerceAPI.BusinessLayer.IHelpers;
 using Microsoft.EntityFrameworkCore;
 using UnitTestingLearn.DataLayer;
+using UnitTestingLearn.DataLayer.Repositories.Class;
+using UnitTestingLearn.DataLayer.Repositories.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddControllers();
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddAutoMapper(typeof(Program));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
